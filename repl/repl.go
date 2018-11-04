077b4037fb765b6c1124f0d8b2fed366b9f50c4a
nrtkbb <nrtkbb@gmail.com>
2018-11-04 15:25:04 +0900

    upgrade repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/nrtkbb/go-MEL/lexer"
-	"github.com/nrtkbb/go-MEL/token"
+	"github.com/nrtkbb/go-MEL/parser"
 )
 
 const prompt = ">> "
@@ -24,9 +24,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printPaserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printPaserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
