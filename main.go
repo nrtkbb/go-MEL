e3e20e97910f298732448b801544a522d6f94583
nrtkbb <nrtkbb@gmail.com>
2018-11-15 01:44:21 +0900

    edit main.go for file parse.

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/user"
 
+	"github.com/nrtkbb/go-MEL/lexer"
+	"github.com/nrtkbb/go-MEL/parser"
 	"github.com/nrtkbb/go-MEL/repl"
 )
 
 func main() {
-	usr, err := user.Current()
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		// REPL mode
+		usr, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Maya Embbeded Language!\n",
+			usr.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+		return
 	}
-	fmt.Printf("Hello %s! This is the Maya Embbeded Language!\n",
-		usr.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
+	filePaths := flag.Args()
+	for _, fp := range filePaths {
+		if _, err := os.Stat(fp); err != nil && !os.IsExist(err) {
+			log.Fatal(err)
+		}
+
+		fmt.Println(fp)
+		input, err := ioutil.ReadFile(fp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		l := lexer.New(string(input))
+		p := parser.New(l)
+		program := p.ParseProgram()
+		fmt.Println(program.String())
+	}
 }
